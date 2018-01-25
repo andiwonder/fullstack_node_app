9d2f080b850f4fe428a2872718ad0d3c680051db
Swapnil Kasliwal <skasliwal12@gmail.com>
2018-01-25 13:32:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// es2015 modules
+// import express from 'express';
+
+//common js modules
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
