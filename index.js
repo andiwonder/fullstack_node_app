1bf6ea19f8e39cc206b4f0d95063bc60a79c5a82
Swapnil Kasliwal <skasliwal12@gmail.com>
2018-01-25 22:55:47 -0500

    google oauth setup

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 
 //common js modules
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-goggle-oath20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(new GoogleStrategy());
 
-
+// for heroku
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
